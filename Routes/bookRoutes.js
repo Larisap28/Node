e76b3ed19881dfbfc35b937b96a1fdec48deca07
Larisap28 <cpaulalarisa@gmail.com>
2015-07-27 09:53:09 +0300

    add changes on bookRoutes

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -1,83 +1,136 @@
 var express = require('express');
 
-var routes = function (Book) {       //routes ca functie pt ca daca vreau sa adaug ceva apoi pt testare pot sa o fac mai usor
+
+var routes = function(Book) {
     var bookRouter = express.Router();
+//create a couple of routes
+    bookRouter. route('/')
+        //get route
 
-    bookRouter.route('/')
+        //POST cu care adaug informatii
+        .post(function(req, res){
+            var book = new Book(req.body); // de tipul Book aleia din bookModel->din mongoose
 
-        .post(function (req, res) {
-            var book = new Book(req.body);
-
+            //console.log(book);
             book.save();
             res.status(201).send(book);
 
         })
-        .get(function (req, res) {
-
+        .get(function(req, res){
             var query = {};
-            if (req.query.genre) {
+            if(req.query.genre)
+            {
                 query.genre = req.query.genre;
             }
-            Book.find(query, function (err, books) {    //book is an instance of our book schema; takes a json obj as a parameter
+
+            //send some data back
+            Book.find(query, function (err, books) {
                 if (err)
                     res.status(500).send(err);
                 else
                     res.json(books);
-            });                               //when you go to /api/books we're gonna do a find and just take the results and pass that back to our browser
-        });
-    bookRouter.use(':/bookId', function (req, res, next) {     //that says, hey, I'm about to do a middleware and I'm going to use only for the rute that has a book id
-        Book.findById(req.params.bookId, function (err, book) {    //book is an instance of our book schema; takes a json obj as a parameter
-            if (err)
-                res.status(500).send(err);
-            else if (book) {
-                req.book = book;
-                next();
-            }
-            else {
-                res.status(404).send('no book found');
-            }
-        });
-    });
-    bookRouter.route('/:bookId')
-        .get(function (req, res) {
 
-            res.json(req.book);
-
-            //when you go to /api/books we're gonna do a find and just take the results and pass that back to our browser
-        })
-
-        .put(function (req, res) {
-            req.book.title = req.body.title;
-            req.book.author = req.body.author;
-            req.book.genre = req.body.genre;
-            req.book.read = req.body.read;    //everything is in req.book is modified with whatever is in req.body
-            req.book.save(function(err) {
-                if (err)
-                    res.status(500).send(err);
-                else {
-                    res.json(req.book);
-                }
             });
-            //res.json(req.book);
-        })
 
-    .patch(function(req,res){          //we don't want to update the id
-        if(req.body._id)
-            delete req.body._id;
+        });
 
-        for (var p in req.body)
-        {
-            req.book[p] = req.body[p];
-        }
-        req.book.save(function(err){
+
+    var getBookById = function(bookId,callback){
+        Book.findById(bookId, function(err, book){
             if(err)
                 res.status(500).send(err);
-            else{
-                res.json(req.book);
+            else if (book)  //daca cartea exista
+            {
+                callback(book);
+
+
             }
+            else{
+                res.status(404).send('no book found');
+            }
+
         });
-    })
+    };
+
+
+    bookRouter.route('/:bookId')
+
+
+        .get(function(req, res){
+            getBookById(req.params.bookId, function(book){
+                res.json(book);
+            });
+        })
+
+        //PUT care imi face update sau imi inlocuieste doar la o parte din resursa
+        .put(function(req, res){
+
+
+            getBookById(req.params.bookId, function(book){
+
+
+
+
+                book.title = req.body.title; //tot ce era in req.book a fost inlocuit cu ce era in req.body
+                book.author = req.body.author;
+                book.genre = req.body.genre;
+                book.read = req.body.read;
+
+                req.book = book;
+                book.save(function(err){
+                    if(err)
+                        res.status(500).send(err);
+                    else{
+                        req.book = book;
+                        res.json(req.book);
+
+                    }
+                });
+            });
+
+
+
+        })
+
+        //PATCH ->care face update doar la o parte din resursa
+        .patch(function(req, res){//vrem sa updatam req.book cu itemii din req.body care sunt acolo
+            //if(req.body.title) //nu folosim asta because this would become very painful, ci vom folosi for in loop pt ca e mai avantajos
+            //{
+            //    req.book.title = req.body.title;
+            //}
+            if(req.body._id)//nu vrem sa updatam si id
+                delete req.body._id;
+            //for every key in req.body = for in loop //e e chivalent cu if-urile alea, dar ar fi trebuit sa fac pt fiecare atribut
+            for(var p in req.body)
+            {
+
+                req.book[p] = req.body[p];
+            }
+            req.book.save(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else{
+                    res.json(req.book);
+
+                }
+            });
+
+        })
+
+        //DELETE -> care imi sterge
+        .delete(function(req, res){
+            req.book.remove(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else {
+                    res.status(204).send('Removed'); //204 inseamna ca a fost sters
+                }
+
+            });
+
+        });   //avem un .get si un .put pt ruta noastra, acum trebuie sa adaugam .patch
+
     return bookRouter;
-};
+}; //return value for this module
 
 module.exports = routes;
